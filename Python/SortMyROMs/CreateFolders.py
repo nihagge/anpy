cac0b9f5912e181bae6f6e70e8acd20f1002dcba
NilsHagge <nils.hagge@seerene.com>
2019-04-24 17:27:39 +0200

    almost ...

--- Python/SortMyROMs/CreateFolders.py
+++ Python/SortMyROMs/CreateFolders.py
@@ -2,34 +2,67 @@
 import os
 filelist =[]
 foldername=[]
-directory="~/devel/Python-Trix/ROMs"
 Access_rights = 0o700
-# list = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+# ABCFolder = ['a', 'b', 'c']
+ABCFolder = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
 
-def CreateFolder():
-    print ("Create new folder:")
+Path1='./PATH1/'
+
+#def CreateGameRomFolders():
+#    # print ("Create new folder:")
+#    for NewFolder in foldername:
+#        try:
+#            os.mkdir(NewFolder, Access_rights)
+#        except OSError:
+#           pass
+#    print ("done")
+
+
+def CreateGameRomFolders():
+    # print ("Create new folder:")
     for NewFolder in foldername:
         try:
             os.mkdir(NewFolder, Access_rights)
+            for SortFolder in ABCFolder:
+                try:
+                    PathToCreate=(Path1,SortFolder)
+                    print ("here is comes: ", SortFolder)
+                    print ("deeper path", PathToCreate)
+                    os.mkdir(os.path.join(NewFolder,SortFolder))
+                except:
+                    pass
         except OSError:
-            print ("Create of the folder %s failed" % NewFolder)
-        else:
-            print ("done")
-    print (directory)
+           pass
+    print ("done")
 
 def ListFolders():
-    print ("list all folders ...")
+    # print ("list all folders ...")
     for x in os.listdir("."):
         if os.path.isfile(x):
-            print ("File found", x)
+           # print ("File found", x)
             filelist.append(x)
     for suffix in filelist:
         suffix1=suffix.split(".")
         foldername.append(suffix1[1])
     print (foldername)
 
+def CreateABCFolder():
+    print ("A B C D E F")
+    for SortFolder in ABCFolder:
+        try:
+            PathToCreate=(Path1,SortFolder)
+            print ("here is comes: ", SortFolder)
+            print ("deeper path", PathToCreate)
+            os.mkdir(os.path.join(Path1,SortFolder))
+        except:
+            pass
+
+# CreateABCFolder()
 ListFolders()
-CreateFolder()
+CreateGameRomFolders()
 
 
-
+#root_path = '/whatever/your/root/path/is/'
+#folders = ['Folder_1','Folder_x','Folder_y']
+#for folder in folders:
+#    os.mkdir(os.path.join(root_path,folder))
